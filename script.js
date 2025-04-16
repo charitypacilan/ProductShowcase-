8a5e6f56839b93ac8fbdb5b32c76d3b76c6e5b55
charitypacilan <chro.pacilan.coc@phinmaed.com>
2025-04-16 23:03:15 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to redirect to the product listing page
+function viewAllProducts() {
+    window.location.href = "products.html";  //redirection link sa view all products na button
+  }
